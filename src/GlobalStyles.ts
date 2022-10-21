95daa61ce66a804f0bb749ac95318c0f954db2e2
piacib <ben.piacitelli@gmail.com>
2022-10-21 09:16:51 -1000

    mirgated files from old project

---
+++ src/GlobalStyles.ts
@@ -0,0 +1,41 @@
+import { createGlobalStyle } from "styled-components";
+export const GlobalStyle = createGlobalStyle`
+@font-face {
+    font-family: "VT323";
+    font-weight: 400;
+    font-style: normal;
+    src: url("fonts/VT323-Regular.woff2") format("woff2"),
+    url("fonts/VT323-Regular.woff") format("woff");
+  }
+html {
+  font-size: 20px;
+}
+body {
+  margin: 0;
+  width: 650px;
+
+}
+button {
+  font-family: inherit;
+}
+#react-root {
+  font-family: 'VT323', monospace;
+  font-size: 1rem;
+  }
+#react {
+   display: flex;
+  align-items: center;
+  justify-content: center;
+  width: calc(100% - calc(2 * ${(props) => props.theme.outerBorderWidth}));
+  height: 100%;
+  border: ${(props) => `${props.theme.outerBorderWidth}
+   ${props.theme.outerBorderColor}
+   ${props.theme.outerBorderStyle}`};
+  background-color: ${(props) => props.theme.backgroundColor};
+
+}
+h1 {
+  font-family: var(--font-family);
+  font-size:2.7rem;
+}
+`;
