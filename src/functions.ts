48c6d4a048a306004c3a0fafbcd5785f40f8f47c
piacib <ben.piacitelli@gmail.com>
2022-11-10 13:35:53 -1000

    added functions to seperate file

--- src/functions.ts
+++ src/functions.ts
@@ -37,3 +37,32 @@ export const dexSearchPrepper = (str: string): string => {
 export const isURLShowdown = (url: string) => {
   return url.includes("play.pokemonshowdown.com");
 };
+const moveFetchPrepper = (move: string) => {
+  return move.replace(" ", "-").toLowerCase();
+};
+interface MoveResponse {
+  move: string;
+  type: string;
+  category: string;
+  power: number;
+  accuracy: number;
+}
+export const getMoveData = (data: string[] | null) => {
+  const dataSet: Array<MoveResponse> = [];
+  if (data) {
+    data.map((move: string) =>
+      fetch(`https://pokeapi.co/api/v2/move/${moveFetchPrepper(move)}`)
+        .then((resp) => resp.json())
+        .then((json) =>
+          dataSet.push({
+            move: json.name,
+            type: json.type.name,
+            category: json.damage_class.name,
+            power: json.power,
+            accuracy: json.accuracy,
+          })
+        )
+    );
+  }
+  return dataSet;
+};
