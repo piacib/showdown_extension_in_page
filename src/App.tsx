cea27596dbb60a21d37473aeed4771406984fc69
piacib <ben.piacitelli@gmail.com>
2022-11-10 13:31:01 -1000

    moved functions and removed unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,24 @@
-import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
+/*global chrome*/
+import React, { useState } from "react";
 import "./App.css";
 import { TypeWriterContainer } from "./TypeWriterContainer.style";
 import { AppDisplay, Button } from "./App.styles";
 import { TeamDisplay } from "./components/TeamDisplay/TeamDisplay";
 import { isDevelopmentMode } from "./functions";
 import useResizeObserver from "use-resize-observer";
+const config = {
+  childList: true,
+  subtree: true,
+};
 
-interface MoveResponse {
-  move: string;
-  type: string;
-  category: string;
-  power: number;
-  accuracy: number;
-}
-const moveFetchPrepper = (move: string) => {
-  return move.replace(" ", "-").toLowerCase();
-};
-export const getMoveData = (data: string[] | null) => {
-  const dataSet: Array<MoveResponse> = [];
-  if (data) {
-    data.map((move: string) =>
-      fetch(`https://pokeapi.co/api/v2/move/${moveFetchPrepper(move)}`)
-        .then((resp) => resp.json())
-        .then((json) =>
-          dataSet.push({
-            move: json.name,
-            type: json.type.name,
-            category: json.damage_class.name,
-            power: json.power,
-            accuracy: json.accuracy,
-          })
-        )
-    );
-  }
-  return dataSet;
-};
 const displayCutOff = 600;
-function App() {
+export interface AppProps {
+  /** room-battle-${string}-${number} */
+  roomId: string;
+}
+const App: React.FC<AppProps> = ({ roomId }) => {
+  console.log("hello from extension");
   const [opponentsTeam, setOpponentsTeam] = useState<boolean>(true);
-  const [team, setTeam] = useState();
   const [changeDisplay, setChangeDisplay] = useState<boolean>(false);
   const { ref, width, height } = useResizeObserver<HTMLDivElement>({
     onResize: ({ width, height }) => {
@@ -54,23 +34,6 @@ function App() {
       }
     },
   });
-
-  console.log("hello from extension");
-  const [randomBattle, setRandomBattle] = useState<string | false>(
-    isDevelopmentMode ? "gen8randomdoublesbattle" : false
-  );
-  useEffect(() => {
-    const path = window.location.pathname;
-    console.log(path);
-    if (path.includes("random")) {
-      const match = path.match(/(?<=\-).+?(?=\-)/);
-      if (match && match[0]) {
-        setRandomBattle(match[0]);
-      }
-    }
-  }, []);
-
-  console.log("randomBattle", randomBattle);
   return (
     <AppDisplay changeDisplay={changeDisplay} ref={ref}>
       <Button onClick={() => setOpponentsTeam(!opponentsTeam)}>
@@ -79,12 +42,9 @@ function App() {
       <TypeWriterContainer>
         <h1>Poke Info</h1>
       </TypeWriterContainer>
-      <TeamDisplay
-        opponentsTeam={opponentsTeam}
-        isRandomBattle={randomBattle}
-      />
+      <TeamDisplay roomId={roomId} opponentsTeam={opponentsTeam} />
     </AppDisplay>
   );
-}
+};
 
 export default App;
