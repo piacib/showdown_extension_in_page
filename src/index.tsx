5447ea477d079047694fee6a90034fd1a0f225c9
piacib <ben.piacitelli@gmail.com>
2022-11-10 13:36:21 -1000

    adjusted root appending to add to each battle room

--- src/index.tsx
+++ src/index.tsx
@@ -5,54 +5,119 @@ import { theme } from "./theme";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./GlobalStyles";
 import { isDevelopmentMode } from "./functions";
-
-// if (isDevelopmentMode) {
-//   const battle = document.createElement("div");
-//   battle.className = "battle-log";
-//   document.body.appendChild(battle);
-// }
-const addDisplay = () => {
+import "./locationChange.js";
+const addDisplay = (battleRoom: HTMLElement) => {
+  console.log("adding display", battleRoom);
+  if (!battleRoom) {
+    // not in battle, return early
+    return;
+  }
+  /** room-battle-${string}-${number} */
+  const roomId: String = battleRoom.id;
+  if (!roomId.startsWith("room-battle")) {
+    return;
+  }
+  const appId = `react-root-${roomId}`;
+  const rootEl = document.getElementById(appId);
+  if (rootEl) {
+    // element is already added, return early
+    return;
+  }
   const app = document.createElement("div");
-  app.id = "react-root";
-  const battleLog = document.getElementsByClassName("battle-log");
-
-  // Make sure the element that you want to mount the app to has loaded. You can
-  // also use `append` or insert the app using another method:
-  // https://developer.mozilla.org/en-US/docs/Web/API/Element#methods
-  //
-  // Also control when the content script is injected from the manifest.json:
-  // https://developer.chrome.com/docs/extensions/mv3/content_scripts/#run_time
+  app.id = appId;
+  console.log("battleRoom", battleRoom);
+  const battleLog = battleRoom.getElementsByClassName("battle-log");
   if (battleLog && battleLog[0]) {
     battleLog[0].prepend(app);
   }
+  const container = document.getElementById(appId);
 
-  console.log(window.location.pathname);
-
-  const container = document.getElementById("react-root");
   const root = createRoot(container!);
-
   root.render(
     <>
       <GlobalStyle theme={theme} />
       <ThemeProvider theme={theme}>
-        <App />
+        <App roomId={battleRoom.id} />
       </ThemeProvider>
     </>
   );
 };
-if (window.location.pathname === "/" && !isDevelopmentMode) {
-  let timer = setInterval(() => {
-    console.log("checking pathname");
-    if (
-      window.location.pathname !== "/" &&
-      window.location.host === "play.pokemonshowdown.com"
-    ) {
-      console.log(window.location.pathname);
-      clearInterval(timer);
-      addDisplay();
+
+const config = {
+  childList: true,
+  subtree: true,
+};
+const getBattleRoomID = (pathname: string) => {
+  // takes in string returns what follows the last "/"
+  const regexMatch = pathname.match(/(?<=-)(?:.(?!-))+$/);
+  if (regexMatch && regexMatch[0]) {
+    return regexMatch[0];
+  }
+  return "";
+};
+let activeBattleRooms: string[] = [];
+const resetBattleRooms = () => {
+  console.log("resetting");
+  const innerUlQuery = document.querySelector(
+    "#header > div.tabbar.maintabbar > div"
+  );
+  if (innerUlQuery?.childElementCount === 2) {
+    // no active battles exist and array should be cleared
+    activeBattleRooms = [];
+  }
+  const battlesUl = innerUlQuery?.children[1];
+  if (!battlesUl) {
+    return;
+  }
+  const battleLi = Array.from(battlesUl.children);
+  const activeBattles: string[] = battleLi.map((li) => {
+    const anchorEl = li.children[0];
+    if (anchorEl instanceof HTMLAnchorElement) {
+      return getBattleRoomID(anchorEl.href);
+    } else {
+      return "";
     }
-  }, 5000);
-} else {
-  console.log("mounting extension");
-  addDisplay();
-}
+  });
+  activeBattleRooms = activeBattles;
+};
+
+const checkBattleRooms = (roomId: string) => {
+  if (activeBattleRooms.includes(roomId)) {
+    return false;
+  }
+  activeBattleRooms.push(roomId);
+  const battleRoom = document.getElementById(
+    "room-" + document.location.pathname.slice(1)
+  );
+  if (battleRoom) {
+    addDisplay(battleRoom);
+  }
+  return true;
+};
+
+// // checks for tab changes
+window.addEventListener("load", () => {
+  let currentPathname = document.location.pathname;
+  let bodyList = document.querySelector("body");
+
+  // checks mutations for a different pathname
+  const observer = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
+      if (currentPathname != document.location.pathname) {
+        console.log("pathchanged", getBattleRoomID(document.location.pathname));
+        currentPathname = document.location.pathname;
+        const roomId = getBattleRoomID(document.location.pathname);
+        if (roomId) {
+          checkBattleRooms(roomId);
+          console.log("battleRooms", activeBattleRooms);
+        } else {
+          resetBattleRooms();
+        }
+      }
+    });
+  });
+  if (bodyList) {
+    console.log("load observer added");
+    observer.observe(bodyList, config);
+  }
+});
