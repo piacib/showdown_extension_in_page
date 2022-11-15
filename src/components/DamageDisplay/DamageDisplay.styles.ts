53429ab615cfcfc4276ae96be1d3aed1e3597af9
piacib <ben.piacitelli@gmail.com>
2022-11-15 12:53:24 -1000

    moved styles to effectivenessDisplay.style

--- src/components/DamageDisplay/DamageDisplay.styles.ts
+++ src/components/DamageDisplay/DamageDisplay.styles.ts
@@ -1,6 +1,6 @@
-import styled from 'styled-components';
-import { typeColorConverter } from '../PokemonDataDisplay/pokemonTypeColorConverter';
-import { TypeColorInterface } from '../../types';
+import styled from "styled-components";
+import { typeColorConverter } from "../PokemonDataDisplay/pokemonTypeColorConverter";
+import { TypeColorInterface } from "../../types";
 
 export const DamageContainer = styled.div`
   grid-row: 4;
@@ -8,25 +8,6 @@ export const DamageContainer = styled.div`
   flex-direction: column;
   justify-content: center;
   font-size: 0.95rem;
-`;
-export const DamageGroupContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: space-around;
-`;
-export const TypeBox = styled.div<TypeColorInterface>`
-  background-color: ${(props) => typeColorConverter[props.background]};
-  margin: 5px;
-
-  padding: ${(props) => props.theme.padding.small} ${(props) => props.theme.padding.medium};
-  border-radius: 10px;
-`;
-export const TypeBoxContainer = styled.div`
-  height: 100%;
-  width: 280px;
-  display: flex;
-  flex-wrap: wrap;
-  align-items: center;
 `;
 export const DamageMultiplier = styled.p`
   height: 100%;
