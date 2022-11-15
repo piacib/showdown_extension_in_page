f3e1b1d7e56043fc8a6a26bcc8262cda0b3e98cc
piacib <ben.piacitelli@gmail.com>
2022-11-15 13:19:42 -1000

    moved all devmode func to one file

---
+++ src/developmentMode.ts
@@ -0,0 +1,4 @@
+export const isDevelopmentMode = process.env.NODE_ENV === "development";
+export const devPathname = "battle-gen8randombattle-1710900595";
+export const devRoomId = "room-" + devPathname;
+export const testTeam = ["slowking", "espeon", "umbreon", "vaporeon", "leafeon", "flareon"];
