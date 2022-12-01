0c216f54d2d6fcaa340428ca7b02cb08d7565629
piacib <ben.piacitelli@gmail.com>
2022-11-30 14:31:40 -1000

    moved functions to function.ts

--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,10 @@ import { theme } from "./theme";
 import { ThemeProvider } from "styled-components";
 
 import "./locationChange.js";
-import { devRoomId, isDevelopmentMode } from "./developmentMode";
 import FontStyles from "./FontStyles";
 import { GlobalStyles } from "./GlobalStyles";
+import { observerConfig, getBattleRoomID, createContainer } from "./functions";
+import { devRoomId, isDevelopmentMode } from "./developmentMode";
 console.log("isDevelopmentMode", isDevelopmentMode);
 if (isDevelopmentMode) {
   const root = createRoot(document.body as HTMLElement);
@@ -15,7 +16,6 @@ if (isDevelopmentMode) {
   root.render(
     <React.StrictMode>
       <GlobalStyles />
-
       <FontStyles />
       <ThemeProvider theme={theme}>
         <App roomId={devRoomId} />
@@ -23,6 +23,7 @@ if (isDevelopmentMode) {
     </React.StrictMode>
   );
 }
+
 const addDisplay = (battleRoom: HTMLElement) => {
   console.log("adding display", battleRoom);
   if (!battleRoom) {
@@ -30,23 +31,11 @@ const addDisplay = (battleRoom: HTMLElement) => {
     return;
   }
   /** room-battle-${string}-${number} */
-  const roomId: String = battleRoom.id;
+  const roomId: string = battleRoom.id;
   if (!roomId.startsWith("room-battle")) {
     return;
   }
-  const appId = `react-root-${roomId}`;
-  const rootEl = document.getElementById(appId);
-  if (rootEl) {
-    // element is already added, return early
-    return;
-  }
-  const app = document.createElement("div");
-  app.id = appId;
-  const battleLog = battleRoom.getElementsByClassName("battle-log");
-  if (battleLog && battleLog[0]) {
-    battleLog[0].prepend(app);
-  }
-  const container = document.getElementById(appId);
+  const container = createContainer(roomId, battleRoom);
 
   const root = createRoot(container!);
   root.render(
@@ -57,19 +46,6 @@ const addDisplay = (battleRoom: HTMLElement) => {
       </ThemeProvider>
     </>
   );
-};
-
-const config = {
-  childList: true,
-  subtree: true,
-};
-const getBattleRoomID = (pathname: string) => {
-  // takes in string returns what follows the last "/"
-  const regexMatch = pathname.match(/(?<=-)(?:.(?!-))+$/);
-  if (regexMatch && regexMatch[0]) {
-    return regexMatch[0];
-  }
-  return "";
 };
 let activeBattleRooms: string[] = [];
 const resetBattleRooms = () => {
@@ -93,7 +69,6 @@ const resetBattleRooms = () => {
   });
   activeBattleRooms = activeBattles;
 };
-
 const checkBattleRooms = (roomId: string) => {
   if (activeBattleRooms.includes(roomId)) {
     return false;
@@ -105,8 +80,8 @@ const checkBattleRooms = (roomId: string) => {
   }
   return true;
 };
-
 // // checks for tab changes
+// let activeBattleRooms: string[] = [];
 window.addEventListener("load", () => {
   let currentPathname = document.location.pathname;
   let body = document.querySelector("body");
@@ -119,7 +94,7 @@ window.addEventListener("load", () => {
         const roomId = getBattleRoomID(document.location.pathname);
         if (roomId) {
           checkBattleRooms(roomId);
-          console.log("battleRooms", activeBattleRooms);
+          // console.log("battleRooms", activeBattleRooms);
         } else {
           resetBattleRooms();
         }
@@ -128,6 +103,6 @@ window.addEventListener("load", () => {
   });
   if (body) {
     console.log("load observer added");
-    observer.observe(body, config);
+    observer.observe(body, observerConfig);
   }
 });
