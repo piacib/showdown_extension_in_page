e296e7d214b463fe48626d33a6a534535eeeabed
piacib <ben.piacitelli@gmail.com>
2022-12-12 13:54:27 -1000

    added type check

--- src/components/MovesDisplay/MovesDisplay.tsx
+++ src/components/MovesDisplay/MovesDisplay.tsx
@@ -13,28 +13,30 @@ import { MoveData } from "@pkmn/dex-types";
 interface MovesDisplayProps {
   moves: MoveData[];
 }
+/**  moves: MoveData[]  @pkmn/dex-types */
 const MovesDisplay: React.FC<MovesDisplayProps> = ({ moves }) => {
   console.log(moves);
   return (
     <MovesContainer>
       Moves:
-      {moves.map((move) => (
-        <MoveBtn background={move.type} key={move.name}>
-          {move.name}
-          <HiddenPropertyText>
-            <MoveInfo>
-              <MoveDescription>{move.shortDesc}</MoveDescription>
-              <MoveType background={move.type}>{move.type}</MoveType>
-              {typeof move.accuracy === "number" && (
-                <MoveProperty>Accuracy: {move.accuracy}%</MoveProperty>
-              )}
-              {Boolean(move.priority) && <MoveProperty>Priority: {move.priority}</MoveProperty>}
-              {Boolean(move.basePower) && <MoveProperty>Power: {move.basePower}</MoveProperty>}
-              <MoveProperty>Category: {move.category}</MoveProperty>
-            </MoveInfo>
-          </HiddenPropertyText>
-        </MoveBtn>
-      ))}
+      {moves &&
+        moves.map((move) => (
+          <MoveBtn background={move.type} key={move.name}>
+            {move.name}
+            <HiddenPropertyText>
+              <MoveInfo>
+                <MoveDescription>{move?.shortDesc}</MoveDescription>
+                <MoveType background={move.type}>{move.type}</MoveType>
+                {typeof move.accuracy === "number" && (
+                  <MoveProperty>Accuracy: {move.accuracy}%</MoveProperty>
+                )}
+                {Boolean(move.priority) && <MoveProperty>Priority: {move.priority}</MoveProperty>}
+                {Boolean(move.basePower) && <MoveProperty>Power: {move.basePower}</MoveProperty>}
+                <MoveProperty>Category: {move.category}</MoveProperty>
+              </MoveInfo>
+            </HiddenPropertyText>
+          </MoveBtn>
+        ))}
     </MovesContainer>
   );
 };
