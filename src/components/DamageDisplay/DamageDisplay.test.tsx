a373c65a3a37e19524ad8a323207b85021a7d024
piacib <ben.piacitelli@gmail.com>
2022-12-12 14:01:51 -1000

    removed file

--- src/components/DamageDisplay/DamageDisplay.test.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import DamageDisplay from "./DamageDisplay";
-import { TypeNamesArr } from "../../types";
-test("renders learn react link", () => {
-  render(<DamageDisplay typesArray={[TypeNamesArr[3], TypeNamesArr[5]]} />);
-});
