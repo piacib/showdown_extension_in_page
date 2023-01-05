c1896e6d1bd0fd03e6abeb104cde771ba456c6ba
piacib <ben.piacitelli@gmail.com>
2023-01-04 14:09:47 -1000

    adjusted devPathanem

--- src/developmentMode.ts
+++ src/developmentMode.ts
@@ -1,5 +1,5 @@
 export const isDevelopmentMode = process.env.NODE_ENV === "development";
-export const devPathname = "battle-gen8randombattle-1710900595";
+export const devPathname = "battle-gen9randombattle-1710900595";
 export const devRoomId = "room-" + devPathname;
 export const userTestTeam = [
   "slowking",
