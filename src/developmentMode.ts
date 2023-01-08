2193690d53b7e95643f925f1f7f0ba1f07e0e1e4
piacib <ben.piacitelli@gmail.com>
2023-01-08 06:54:39 -1000

    changed test pokemon

--- src/developmentMode.ts
+++ src/developmentMode.ts
@@ -1,20 +1,13 @@
 export const isDevelopmentMode = process.env.NODE_ENV === "development";
 export const devPathname = "battle-gen9randombattle-1710900595";
 export const devRoomId = "room-" + devPathname;
-export const userTestTeam = [
-  "slowking",
-  "Abomasnow",
-  "Arctozolt",
-  "Bouffalant",
-  "Gyarados",
-  "Giratina",
-];
+export const userTestTeam = ["Altaria", "Abomasnow", "Flapple", "Drifblim", "Gyarados", "Giratina"];
 export const opponentTestTeam = [
   "slowking",
-  "Kabutops",
+  "Carbink",
   "Azumarill",
   "Gallade",
-  "Genesect-Douse",
+  "Abomasnow",
   "Diancie",
 ];
 
