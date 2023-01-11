9bdfaa6c5c230b0a78b09efcaea2f7a0476fd082
piacib <ben.piacitelli@gmail.com>
2023-01-11 10:15:55 -1000

    moved items into an h3

--- src/components/ItemsDisplay/ItemsDisplay.tsx
+++ src/components/ItemsDisplay/ItemsDisplay.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import { ItemsContainer } from "./Items.style";
-import {
-  PropertyBtn,
-  HiddenPropertyText,
-} from "../PokemonDataDisplay/DataDisplay.style";
+import { PropertyBtn, HiddenPropertyText } from "../PokemonDataDisplay/DataDisplay.style";
 import { Dex } from "@pkmn/dex";
 import { dexSearchPrepper } from "../../functions";
 
@@ -14,13 +11,11 @@ interface ItemsDisplayProps {
 const ItemsDisplay: React.FC<ItemsDisplayProps> = ({ items }) => {
   return (
     <ItemsContainer>
-      Items:
+      <h3>Items:</h3>
       {items.map((item) => (
         <PropertyBtn key={item}>
           {item}
-          <HiddenPropertyText>
-            {Items[dexSearchPrepper(item)].desc}
-          </HiddenPropertyText>
+          <HiddenPropertyText>{Items[dexSearchPrepper(item)].desc}</HiddenPropertyText>
         </PropertyBtn>
       ))}
     </ItemsContainer>
